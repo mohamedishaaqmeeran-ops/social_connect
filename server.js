76495a97cceebd23537a31c0bc1e21ac5c47817e
unknown <mohamedishaaqmeeran@gmail.com>
2026-06-28 09:47:08 +0530

    update

--- server.js
+++ server.js
@@ -13,9 +13,23 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "https://twinn.live",
+  "https://www.twinn.live",
+  "https://social-connect-om87.onrender.com",
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
